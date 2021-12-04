9addc38b5a36a7cc1062f96d59a501e84d907ff1
Kristakri <nago.gun@mail.ru>
2021-12-04 15:19:51 +0300

    Nihao

--- Введение/function.js
+++ Введение/function.js
@@ -1,4 +1,4 @@
 const upper = (text) => text.charAt(0).toUpperCase() + text.slice(1);
-const welcome = ({ name }) => `Konichiva, ${upper(name)}`;
+const welcome = ({ name }) => `Nihao, ${upper(name)}`;
 
 console.log(welcome({ name: "alex" }));
